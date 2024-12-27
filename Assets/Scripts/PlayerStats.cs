8d4fa9f3bc99a66bcf4ee18f4ade43d8453cefe8
lledinh <lledinh@users.noreply.github.com>
2024-12-27 22:18:02 +0100

    Add stats scripts

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PlayerStats : ScriptableObject
+{
+    public Stat speed;
+    public Stat fishing;
+    public Stat fishingChance;
+    public Stat level;
+}
---
+++ Assets/Scripts/Stat.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+[Serializable]
+public class Stat
+{
+    [SerializeField]
+    private float value;
+
+    public float GetValue()
+    {
+        return value;
+    }
+
+    public void Increase(float v)
+    {
+        value += v;
+    }
+
+    public void Decrease(float v)
+    {
+        value -= v;
+    }
+}
